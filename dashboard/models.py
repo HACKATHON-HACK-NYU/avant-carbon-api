053b67944f58f3689abbc61522a83335edd3250a
Timothy Xu <xu.timothy96@gmail.com>
2022-02-27 03:11:27 -0500

    Update models.py
    Added Transaction model

--- dashboard/models.py
+++ dashboard/models.py
@@ -10,3 +10,14 @@ class Card(models.Model):
 
     def __str__(self):
         return str(self.id)
+  
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    # the card this transaction is on
+    card_number = models.CharField(max_length=100,blank=True,null=True)
+    date = models.DateField()
+    merchant_description = models.CharField(max_length = 30)
+    amount = models.DecimalField(decimal_places=2,max_digits=99)	
+    
+    def __str__(self):
+        return str(self.id)
